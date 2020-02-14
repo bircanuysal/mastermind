5271d2ddaf757ed9ed1ba37ac238f87605ca2e0b
tuvo1106 <37724651+tuvo1106@users.noreply.github.com>
2020-02-13 18:07:59 -0800

    Refactor scoreboard into its own class

--- models/board.py
+++ models/board.py
@@ -2,6 +2,7 @@
 
 from collections import Counter
 from models.player import Player
+from models.scoreboard import Scoreboard
 from random import randint
 from typing import List
 
@@ -20,8 +21,7 @@ class Board:
         self.num_count = num_count
         self.num_combinations = num_combinations
         self.num_list = self.generate_numbers()
-        self.player_guesses = []
-        self.player_scores = []
+        self.scoreboard = Scoreboard(self.num_count)
         print("Answer = " + str(self.num_list))
 
     def generate_numbers(self) -> List[int]:
@@ -36,11 +36,7 @@ class Board:
         Displays the current status of the board. This includes a list of
         the player's previous guesses as well as the player's current score.
         """
-        print(f"=== Board ===")
-        self.player_guesses and print(
-            f"=== Player guesses {self.player_guesses}")
-        self.player_scores and print(
-            f"=== Player score {self.player_scores} ===")
+        print(self.scoreboard)
 
     def get_player_input(self, player: Player):
         """
@@ -48,14 +44,15 @@ class Board:
         """
         guesses = []
         guess_count = 1
+        print(f"{player.turns} turns left.")
         while len(guesses) != self.num_count:
             guess = input(f"Enter in your guess for position {guess_count}: ")
             # TODO
             # Validate user input
             guesses.append(int(guess))
             guess_count += 1
-        self.player_guesses.append(guesses)
         player.turns -= 1
+        self.scoreboard.player_guesses.append(guesses)
 
     def check_board(self):
         """
@@ -63,14 +60,13 @@ class Board:
         Algorithm speed is currently O(n) time complexity and O(n) space
         complexity.
         """
-        last_guesses = self.player_guesses[-1]
-        correct_colors = (len(Counter(self.num_list)
-                              & Counter((last_guesses))))
+        last_guesses = self.scoreboard.player_guesses[-1]
+        correct_colors = sum(
+            (Counter(self.num_list) & Counter(last_guesses)).values())
         correct_positions = 0
         for i in range(len(last_guesses)):
             if last_guesses[i] == self.num_list[i]:
                 correct_positions += 1
                 correct_colors -= 1
-        self.player_scores.append(
-            {"correct_colors": correct_colors,
-             "correct_positions": correct_positions})
+        self.scoreboard.player_scores.append(
+            {"correct_colors": correct_colors, "correct_positions": correct_positions})
