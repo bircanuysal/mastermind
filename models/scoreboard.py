05df2821a758d33afb03c807c5edd02736e9c4d5
tuvo1106 <37724651+tuvo1106@users.noreply.github.com>
2020-02-13 18:06:42 -0800

    Add first draft of Scoreboard class

---
+++ models/scoreboard.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+
+class Scoreboard:
+    """
+    Scoreboard class.
+    """
+
+    def __init__(self, num_count):
+        """
+        ScoreBoard constructor.
+        """
+        self.num_count = num_count
+        self.player_guesses = []
+        self.player_scores = []
+
+    def generate_history(self) -> str:
+        """
+        Generates history of guesses and scores for scoreboard.
+        """
+        scoreboard = []
+        for i, (g, s) in enumerate(zip(self.player_guesses, self.player_scores)):
+            string_builder = []
+            string_builder.append(f"{i + 1}. ")
+            string_builder.append(str([i for i in g]))
+            string_builder.append("\t\t")
+            correct_positions = s.get("correct_positions")
+            string_builder.append("X " * correct_positions)
+            correct_colors = s.get("correct_colors")
+            string_builder.append("O " * correct_colors)
+            string_builder.append(
+                "* " * (self.num_count - int(correct_colors) - int(correct_positions)))
+            scoreboard.append("".join(string_builder))
+        return scoreboard
+
+    def __str__(self) -> str:
+        """
+        String representation of class."""
+        sb = self.generate_history()
+        results = "\n".join([line for line in sb])
+        string = "\n".join(
+            ("===========================",
+             "\nMASTERMIND\n",
+             results,
+             "===========================",))
+        return string
