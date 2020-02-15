8f178393a24bec8a55ee884f4f9d42ed2a22e139
tuvo1106 <37724651+tuvo1106@users.noreply.github.com>
2020-02-14 22:20:09 -0800

    Add first draft of str_validator class

---
+++ models/utils/str_validator.py
@@ -0,0 +1,37 @@
+from models.utils.validator import Validator
+
+
+class StrValidator(Validator):
+    """
+    String validator class.
+    """
+
+    def get_name(self) -> str:
+        """
+        Gets user input for names.
+        """
+        prompt = "What is your name? "
+        return self.get_input(prompt)
+
+    def get_input(self, prompt: str) -> str:
+        """
+        Validates user input to contain only alphabet characters or spaces
+        or will re-prompt user to re-enter.
+        """
+        while True:
+            string = input(prompt).strip()
+            if self.is_alpha_or_space(string):
+                return string
+            else:
+                print("Invalid input. Only alpabet characters and spaces only.")
+
+    def is_alpha_or_space(self, string: str) -> bool:
+        """
+        Determines if given string has only alphabet characters or spaces.
+        """
+        for c in list(string):
+            if c == " ":
+                continue
+            if not c.isalpha():
+                return False
+        return True
