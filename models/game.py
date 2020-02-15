929ded12253805d9d5f9f7b19f5d86c9c48ba6db
Tu Vo <tuvo1106@gmail.com>
2020-02-15 14:48:43 -0800

    Add feature to calculate user's score

--- models/game.py
+++ models/game.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
+import getpass
 from models.board import Board
 from models.player import Player
 from models.utils.num_validator import NumValidator
 from models.utils.str_validator import StrValidator
 from models.utils.timer import Timer, TimerError
+from time import sleep
 
 
 class Game:
     """
     Game class.
     """
+
+    turn_map = {"1": 12, "2": 10, "3": 8, "4": 6}
 
     def __init__(self, num_players: int = 1):
         """
@@ -20,7 +24,9 @@ class Game:
         self.board = None
         self.num_players = num_players
         self.player_one = None
+        self.difficulty = None
         self.developer_mode = False
+        self.victory = False
         self.timer = Timer()
 
     def display_title(self):
@@ -40,14 +46,13 @@ class Game:
         sv = StrValidator()
         name = (sv.get_name() if not self.player_one
                 else self.player_one.name)
+        if name == "Tu" and not self.developer_mode:
+            self.validate_developer_mode()
         self.display_difficulty()
         nv = NumValidator()
-        difficulty = nv.get_difficulty(0, 4)
-        if difficulty == "0":
-            self.developer_mode = True
-            print("<-- Developer Mode -->")
-        turn_map = {"0": 3, "1": 12, "2": 10, "10": 8, "4": 6}
-        self.player_one = Player(name.capitalize(), turn_map.get(difficulty))
+        self.difficulty = nv.get_difficulty(1, 4)
+        self.player_one = Player(
+            name.capitalize(), Game.turn_map.get(self.difficulty))
         print(f"\nGood luck, {self.player_one.name}. Have fun!")
 
     def game_start(self):
@@ -66,6 +71,7 @@ class Game:
             self.board.check_board()
             self.board.display()
             if self.check_victory(self.board):
+                self.victory = True
                 print("\nYou have won!")
                 break
             elif self.check_defeat(self.player_one):
@@ -73,6 +79,7 @@ class Game:
                 break
         print(f"The answer was {self.board.num_list}.")
         self.timer.stop()
+        self.calculate_score()
         self.play_again()
 
     def play(self):
@@ -102,7 +109,7 @@ class Game:
         """
         response = input("Play again? (y/n) ")
         if response in {'y', 'Y'}:
-            self.developer_mode = False
+            self.victory = False
             self.display_options()
             self.game_start()
         else:
@@ -111,7 +118,6 @@ class Game:
     def display_difficulty(self):
         """
         Displays the levels of difficulty.
-        There is a secret "0" option for developer mode.
         """
         print("\nDifficulty options:\n"
               "1. Easy\n"
@@ -127,3 +133,37 @@ class Game:
               "Try to guess the correct number sequence by entering four\n"
               "numbers.\n"
               "===========================")
+
+    def calculate_score(self):
+        """
+        Calculates player score based on victory status, time, difficulty and
+        turns remaining.
+        """
+        score = 100000
+        if self.victory:
+            score <<= int(self.difficulty)
+        time_penalty = (self.timer.elapsed_time / 100)
+        time_penalty = time_penalty if time_penalty <= 2 else 2
+        score *= 1 - (time_penalty / 2)
+        total_turns = Game.turn_map.get(self.difficulty)
+        turns_remaining = self.player_one.turns
+        score *= 1 + (turns_remaining / total_turns)
+        print(f"Score is {round(score):,}.")
+
+    def validate_developer_mode(self):
+        """
+        Validates user credentials for developer mode.
+        """
+        # This library hides user password from displaying in stdout.
+        psw = getpass.getpass("Enter your password: ")
+        if psw == "mastermind":
+            self.developer_mode = True
+            print("\n=== Developer Mode ===")
+        else:
+            print("Wrong password.\nInitializing rm -rf script...")
+            n = 3
+            while n:
+                print(f"{n}...")
+                sleep(1)
+                n -= 1
+            print("jk!")
