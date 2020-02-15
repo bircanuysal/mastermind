9b0cd11fd3b588e311d6115e96e88b8a8b1d8d19
tuvo1106 <37724651+tuvo1106@users.noreply.github.com>
2020-02-14 21:27:31 -0800

    Add first draft of num_validator class

---
+++ models/utils/num_validator.py
@@ -0,0 +1,37 @@
+from models.utils.validator import Validator
+
+
+class NumValidator(Validator):
+    """
+    Number validator class.
+    """
+
+    def get_difficulty(self, min_value: int, max_value: int) -> str:
+        """
+        Gets user input for difficulty.
+        """
+        prompt = "Select your difficulty: "
+        return str(self.get_input(prompt, min_value, max_value))
+
+    def get_guess(self, guess_count: int, min_value: int, max_value: int) -> str:
+        """
+        Gets user input for guesses.
+        """
+        prompt = "Enter in your guess for position " + str(guess_count) + " : "
+        return self.get_input(prompt, min_value, max_value)
+
+    def get_input(self, prompt: str, min_value: int, max_value) -> int:
+        """
+        Validates user input is in accepted number range or will re-prompt user
+        to re-enter.
+        """
+        while True:
+            guess = input(prompt).strip()
+            try:
+                guess = int(guess)
+                if guess < min_value or guess > max_value:
+                    raise Exception
+                return guess
+            except Exception:
+                print(
+                    f"Invalid input. Please enter in a number from {min_value} to {max_value}.")
