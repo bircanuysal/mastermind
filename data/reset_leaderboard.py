d4ee1de60d4d5a09008861afac584d0da54aab52
Tu Vo <tuvo1106@gmail.com>
2020-02-16 13:04:20 -0800

    Move reset script to data folder

--- data/reset_leaderboard.py
+++ data/reset_leaderboard.py
@@ -6,6 +6,6 @@ import os
 if __name__ == "__main__":
     try:
         os.system(
-            "cp ./dev_data/leaderboard_default.json ./dev_data/leaderboard.json")
+            "cp ./leaderboard_default.json ./leaderboard.json")
     except Exception as e:
         print(f"Error:\n{e}")
