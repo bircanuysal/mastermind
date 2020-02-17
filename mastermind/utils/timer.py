c3e81e1de7c5b16121f960928ad948ea41eaa577
Tu Vo <tuvo1106@gmail.com>
2020-02-17 08:24:41 -0800

    Fix spacing and grammer in Timer class

--- mastermind/utils/timer.py
+++ mastermind/utils/timer.py
@@ -26,8 +26,7 @@ class Timer:
         Starts a new timer.
         """
         if self.start_time:
-            raise TimerError(f"Timer is running. Use .stop() to stop it")
-
+            raise TimerError(f"Timer is running. Use .stop() to stop it.")
         self.start_time = time.perf_counter()
 
     def stop(self):
@@ -35,8 +34,8 @@ class Timer:
         Stops the timer, and reports the elapsed time.
         """
         if not self.start_time:
-            raise TimerError(f"Timer is not running. Use .start() to start it")
-
+            raise TimerError(
+                f"Timer is not running. Use .start() to start it.")
         self.elapsed_time = time.perf_counter() - self.start_time
         self.start_time = None
         print(f"Elapsed time: {self.elapsed_time:0.2f} seconds.")
