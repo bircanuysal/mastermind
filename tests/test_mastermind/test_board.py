852ff0766bd92dbc9458e914b9b649575028d019
Tu Vo <tuvo1106@gmail.com>
2020-02-17 13:45:22 -0800

    Add unit tests for Board class

---
+++ tests/test_mastermind/test_board.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+from mastermind.board import Board
+from mastermind.scoreboard import Scoreboard
+import unittest
+
+
+class Board_Test(unittest.TestCase):
+    """
+    Tests for the Board class.
+    """
+
+    def test_generate_numbers_locally(self):
+        """
+        Test generate_numbers_locally() for correct return type and values.
+        """
+        board = Board(7, True)
+        res = board.generate_numbers_locally()
+        self.assertTrue(type(res) == list)
+        self.assertTrue(len(res) == 4)
+        self.assertTrue(all([type(i) == int for i in res]))
+        self.assertTrue(all([7 >= int(i) >= 0 for i in res]))
+
+        board = Board(9, True)
+        res = board.generate_numbers_locally()
+        self.assertTrue(type(res) == list)
+        self.assertTrue(len(res) == 4)
+        self.assertTrue(all([type(i) == int for i in res]))
+        self.assertTrue(all([9 >= int(i) >= 0 for i in res]))
+
+    @unittest.skip("To reduce the chance of going over rate limits.")
+    def test_generate_numbers_with_API(self):
+        """
+        Test generate_numbers_with_API() for correct return type and values.
+        """
+        board = Board(7, True)
+        res = board.generate_numbers_with_API()
+        self.assertTrue(type(res) == list)
+        self.assertTrue(len(res) == 4)
+        self.assertTrue(all([type(i) == int for i in res]))
+        self.assertTrue(all([7 >= int(i) >= 0 for i in res]))
+
+    def test_check_board_00(self):
+        """
+        Test check_board() for correct return type and values.
+        """
+        board = Board(7, True)
+        board.num_list = [1, 2, 3, 4]
+        scoreboard = Scoreboard()
+        board.scoreboard = scoreboard
+        scoreboard.player_guesses = [[1, 2, 3, 4]]
+        self.assertDictEqual(board.check_board(),
+                             {"correct_colors": 0,
+                              "correct_positions": 4})
+
+    def test_check_board_01(self):
+        """
+        Test check_board() for correct return type and values.
+        """
+        board = Board(7, True)
+        board.num_list = [1, 2, 3, 4]
+        scoreboard = Scoreboard()
+        board.scoreboard = scoreboard
+        scoreboard.player_guesses = [[5, 6, 7, 0]]
+        self.assertDictEqual(board.check_board(),
+                             {"correct_colors": 0,
+                              "correct_positions": 0})
+
+    def test_check_board_02(self):
+        """
+        Test check_board() for correct return type and values.
+        """
+        board = Board(7, True)
+        board.num_list = [1, 2, 3, 4]
+        scoreboard = Scoreboard()
+        board.scoreboard = scoreboard
+        scoreboard.player_guesses = [[1, 1, 1, 1]]
+        self.assertDictEqual(board.check_board(),
+                             {"correct_colors": 0,
+                              "correct_positions": 1})
+
+    def test_check_board_03(self):
+        """
+        Test check_board() for correct return type and values.
+        """
+        board = Board(7, True)
+        board.num_list = [1, 2, 3, 4]
+        scoreboard = Scoreboard()
+        board.scoreboard = scoreboard
+        scoreboard.player_guesses = [[1, 4, 2, 3]]
+        self.assertDictEqual(board.check_board(),
+                             {"correct_colors": 3,
+                              "correct_positions": 1})
+
+    def test_check_board_04(self):
+        """
+        Test check_board() for correct return type and values.
+        """
+        board = Board(7, True)
+        board.num_list = [2, 2, 5, 5]
+        scoreboard = Scoreboard()
+        board.scoreboard = scoreboard
+        scoreboard.player_guesses = [[2, 2, 2, 2]]
+        self.assertDictEqual(board.check_board(),
+                             {"correct_colors": 0,
+                              "correct_positions": 2})
+
+    def test_generate_hint_truthy(self):
+        """
+        Test generate_hint() for truthy returns.
+        """
+        board = Board(7, True)
+        self.assertTrue(board.generate_hint())
+
+    def test_generate_hint_falsy(self):
+        """
+        Test generate_hint() for falsy returns.
+        """
+        board = Board(7, True)
+        for _ in range(4):
+            board.generate_hint()
+        self.assertFalse(board.generate_hint())
+
+
+if __name__ == '__main__':
+    unittest.main()
