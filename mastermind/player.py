fca4dbe198251684bb7acb13a461b0916426f69e
Tu Vo <tuvo1106@gmail.com>
2020-02-17 10:43:13 -0800

    Refactor Player class

--- mastermind/player.py
+++ mastermind/player.py
@@ -6,10 +6,11 @@ class Player:
     Player class.
     """
 
-    def __init__(self, name: str = "Tu", turns: int = 10):
+    def __init__(self, name: str, difficulty: int):
         """
         Player constructor.
-        Default value for name is Tu and default value for turns is 10.
         """
+        turn_map = {"1": 12, "2": 10, "3": 8, "4": 6}
         self.name = name
-        self.turns = turns
+        self.difficulty = difficulty
+        self.turns = turn_map.get(self.difficulty)
