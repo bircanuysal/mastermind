daac193043785ebef4c70a2d629f11ffd47eb468
Tu Vo <tuvo1106@gmail.com>
2020-02-18 07:13:23 -0800

    Replace AssertEquals with AssertEqual in Score tests

--- tests/test_mastermind/test_score.py
+++ tests/test_mastermind/test_score.py
@@ -16,19 +16,19 @@ class Score_Test(unittest.TestCase):
         """
         player = Player("Tu", "1")
         score = Score(player, False, 0, 4, 4)
-        self.assertEquals(score.apply_victory_bonus(), 100000)
+        self.assertEqual(score.apply_victory_bonus(), 100000)
         player = Player("Tu", "1")
         score = Score(player, True, 0, 4, 4)
-        self.assertEquals(score.apply_victory_bonus(), 200000)
+        self.assertEqual(score.apply_victory_bonus(), 200000)
         player = Player("Tu", "2")
         score = Score(player, True, 0, 4, 4)
-        self.assertEquals(score.apply_victory_bonus(), 400000)
+        self.assertEqual(score.apply_victory_bonus(), 400000)
         player = Player("Tu", "3")
         score = Score(player, True, 0, 4, 4)
-        self.assertEquals(score.apply_victory_bonus(), 800000)
+        self.assertEqual(score.apply_victory_bonus(), 800000)
         player = Player("Tu", "4")
         score = Score(player, True, 0, 4, 4)
-        self.assertEquals(score.apply_victory_bonus(), 1600000)
+        self.assertEqual(score.apply_victory_bonus(), 1600000)
 
     def test_apply_time_penalty(self):
         """
@@ -36,15 +36,15 @@ class Score_Test(unittest.TestCase):
         """
         player = Player("Tu", "1")
         score = Score(player, True, 0, 4, 4)
-        self.assertEquals(score.apply_time_penalty(), 100000)
+        self.assertEqual(score.apply_time_penalty(), 100000)
         score = Score(player, True, 50, 4, 4)
-        self.assertEquals(score.apply_time_penalty(), 75000)
+        self.assertEqual(score.apply_time_penalty(), 75000)
         score = Score(player, True, 100, 4, 4)
-        self.assertEquals(score.apply_time_penalty(), 50000)
+        self.assertEqual(score.apply_time_penalty(), 50000)
         score = Score(player, True, 200, 4, 4)
-        self.assertEquals(int(score.apply_time_penalty()), 0)
+        self.assertEqual(int(score.apply_time_penalty()), 0)
         score = Score(player, True, 250, 4, 4)
-        self.assertEquals(int(score.apply_time_penalty()), 0)
+        self.assertEqual(int(score.apply_time_penalty()), 0)
 
     def test_apply_turn_bonus(self):
         """
@@ -53,23 +53,23 @@ class Score_Test(unittest.TestCase):
         player = Player("Tu", "1")
         player.turns = 12
         score = Score(player, True, 0, 4, 4)
-        self.assertEquals(score.apply_turn_bonus(), 200000)
+        self.assertEqual(score.apply_turn_bonus(), 200000)
         player.turns = 6
         score = Score(player, True, 0, 4, 4)
-        self.assertEquals(score.apply_turn_bonus(), 150000)
+        self.assertEqual(score.apply_turn_bonus(), 150000)
         player.turns = 0
         score = Score(player, True, 0, 4, 4)
-        self.assertEquals(score.apply_turn_bonus(), 100000)
+        self.assertEqual(score.apply_turn_bonus(), 100000)
         player = Player("Tu", "4")
         player.turns = 6
         score = Score(player, True, 0, 4, 4)
-        self.assertEquals(score.apply_turn_bonus(), 200000)
+        self.assertEqual(score.apply_turn_bonus(), 200000)
         player.turns = 3
         score = Score(player, True, 0, 4, 4)
-        self.assertEquals(score.apply_turn_bonus(), 150000)
+        self.assertEqual(score.apply_turn_bonus(), 150000)
         player.turns = 0
         score = Score(player, True, 0, 4, 4)
-        self.assertEquals(score.apply_turn_bonus(), 100000)
+        self.assertEqual(score.apply_turn_bonus(), 100000)
 
     def test_apply_hint_penalty(self):
         """
@@ -77,15 +77,15 @@ class Score_Test(unittest.TestCase):
         """
         player = Player("Tu", "1")
         score = Score(player, True, 0, 4, 4)
-        self.assertEquals(score.apply_hint_penalty(), 100000)
+        self.assertEqual(score.apply_hint_penalty(), 100000)
         score = Score(player, True, 0, 4, 3)
-        self.assertEquals(score.apply_hint_penalty(), 75000)
+        self.assertEqual(score.apply_hint_penalty(), 75000)
         score = Score(player, True, 0, 4, 2)
-        self.assertEquals(score.apply_hint_penalty(), 50000)
+        self.assertEqual(score.apply_hint_penalty(), 50000)
         score = Score(player, True, 0, 4, 1)
-        self.assertEquals(score.apply_hint_penalty(), 25000)
+        self.assertEqual(score.apply_hint_penalty(), 25000)
         score = Score(player, True, 0, 4, 0)
-        self.assertEquals(int(score.apply_hint_penalty()), 0)
+        self.assertEqual(int(score.apply_hint_penalty()), 0)
 
 
 if __name__ == '__main__':
