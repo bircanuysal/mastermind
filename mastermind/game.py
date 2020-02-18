2e9494b1d1e0b69429795c2cda72a1a8295f7925
Tu Vo <tuvo1106@gmail.com>
2020-02-18 07:04:37 -0800

    Refactor game class to have separate UI class

--- mastermind/game.py
+++ mastermind/game.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
 
-import getpass
 from mastermind.board import Board
-from mastermind.leaderboard import Leaderboard
-from mastermind.player import Player
-from mastermind.score import Score
-from mastermind.utils.num_validator import NumValidator
-from mastermind.utils.str_validator import StrValidator
+from mastermind.user_interface import UserInterface
 from mastermind.utils.timer import Timer
-from time import sleep
 
 
 class Game:
@@ -20,110 +14,39 @@ class Game:
         """
         Game constructor.
         """
+        self.ui = UserInterface()
         self.board = None
         self.player_one = None
         self.developer_mode = False
 
-    def play(self):
+    def game_on(self):
         """
         Main entry point for game object.
         """
-        self.display_title()
-        self.display_home()
-        self.display_options()
+        self.ui.display_title()
+        self.ui.display_home()
+        self.player_one, self.developer_mode = self.ui.display_player_options(
+            self.player_one, self.developer_mode)
         self.game_start()
-
-    def display_title(self):
-        """
-        Displays title screen.
-        """
-        print("================================================"
-              "\n\nMASTERMIND\n\n"
-              "================================================\n"
-              "Test your code-cracking prowess with Mastermind,\n"
-              "the challenging game of logic and deduction.\n"
-              "================================================\n")
-
-    def display_home(self):
-        """
-        Displays home screen with options for playing the game or viewing
-        the leaderboard.
-        """
-        print("Welcome!\n"
-              "1. Play game\n"
-              "2. View leaderboard\n")
-        nv = NumValidator()
-        option = nv.get_home_screen(1, 2)
-        if option == 2:
-            Leaderboard.display_leaderboard()
-
-    def display_options(self):
-        """
-        Displays options screen.
-        """
-        sv = StrValidator()
-        name = (sv.get_name() if not self.player_one
-                else self.player_one.name)
-        if name == "Tu" and not self.developer_mode:
-            self.validate_developer_mode()
-        self.display_difficulty()
-        nv = NumValidator()
-        difficulty = nv.get_difficulty(1, 4)
-        self.player_one = Player(name, difficulty)
-        print(f"\nGood luck, {self.player_one.name}. Have fun!")
-
-    def validate_developer_mode(self):
-        """
-        Validates user credentials for developer mode.
-        """
-        # This library hides user password from displaying in stdout.
-        psw = getpass.getpass("Enter your password: ")
-        if psw == "mastermind":
-            self.developer_mode = True
-            print("\n=============== Developer Mode ==================")
-        else:
-            print("\nWrong password.\nInitializing rm -rf script...")
-            n = 3
-            while n:
-                print(f"{n}...")
-                sleep(1)
-                n -= 1
-            print("jk!")
-
-    def display_difficulty(self):
-        """
-        Displays the levels of difficulty.
-        """
-        print("\nDifficulty options:\n"
-              "1. Easy - 12 turns.\n"
-              "2. Normal - 10 turns.\n"
-              "3. Hard - 8 turns.\n"
-              "4. Dark Souls - 6 turns. 10 numbers to choose from.\n")
 
     def game_start(self):
         """
         Starts a new game. New board is instantiated.
         """
-        d = self.developer_mode
-        self.board = Board(
-            9, d) if self.player_one.difficulty == "4" else Board(7, d)
-        self.display_instructions()
+        if self.player_one.difficulty == "4":
+            self.board = Board(9, self.developer_mode)
+        else:
+            self.board = Board(7, self.developer_mode)
+        self.ui.display_instructions()
         timer = Timer()
         timer.start()
         victory_status = self.game_loop()
         timer.stop()
         print(f"The answer was {self.board.num_list}.")
-        self.calculate_score(victory_status, timer.elapsed_time)
-        self.end_game()
-
-    def display_instructions(self):
-        """
-        Displays game instructions.
-        """
-        print("\n================================================\n"
-              "Try to guess the correct number sequence by\n"
-              "entering four numbers.\n"
-              "================================================")
+        self.player_one.calculate_score(
+            victory_status, timer.elapsed_time, self.board.num_count,
+            len(self.board.hints_remaining))
+        self.game_end()
 
     def game_loop(self) -> bool:
         """
@@ -131,43 +54,26 @@ class Game:
         false if player has lost.
         """
         while True:
+            print(f"\nGood luck, { self.player_one.name}. Have fun!")
             self.developer_mode and print(
                 "Answer = " + str(self.board.num_list))
             self.board.get_player_input(self.player_one)
             self.board.check_board()
-            self.board.display()
-            if self.player_one.check_victory(self.board.num_count, self.board.scoreboard):
+            self.board.display_history()
+            if self.player_one.check_victory(self.board.num_count,
+                                             self.board.history.player_scores):
                 print("\nYou have won!")
                 return True
             elif self.player_one.check_defeat():
                 print(f"\nBetter luck next time, {self.player_one.name}!")
                 return False
 
-    def calculate_score(self, victory: bool, time: int):
+    def game_end(self):
         """
-        Calculates player score based on victory status, time, difficulty and
-        turns remaining.
+        End game options. Restarts the game if player chooses to play again.
         """
-        s = Score(self.board, self.player_one, victory, time)
-        score = s.final_score()
-        print(f"Your score was {score}.")
-        Leaderboard.write_score(self.player_one.name, score)
-
-    def end_game(self):
-        """
-        Prompts the player to play again, view leaderboard or quit.
-        """
-        nv = NumValidator()
-        while True:
-            print("\nWhat would you like to do next?\n"
-                  "1. Play again.\n"
-                  "2. View leaderboard.\n"
-                  "3. Quit.\n")
-            response = nv.get_endgame(1, 3)
-            if response == 1:
-                self.display_options()
-                return self.game_start()
-            elif response == 2:
-                Leaderboard.display_leaderboard()
-            else:
-                return print(f"Goodbye!")
+        play_again = self.ui.display_endgame()
+        if play_again:
+            self.ui.display_player_options(
+                self.player_one, self.developer_mode)
+            self.game_start()
---
+++ mastermind/user_interface.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+from mastermind.leaderboard import Leaderboard
+from mastermind.player import Player
+from mastermind.utils.auth_validator import AuthValidator
+from mastermind.utils.num_validator import NumValidator
+from mastermind.utils.str_validator import StrValidator
+
+
+class UserInterface:
+    """
+    User interface class.
+    """
+
+    def __init__(self):
+        """
+        User Interface constructor. Initializes all validators as instance
+        attributes.
+        """
+        self.nv = NumValidator()
+        self.sv = StrValidator()
+        self.av = AuthValidator()
+
+    def display_title(self):
+        """
+        Displays title screen.
+        """
+        print("================================================"
+              "\n\nMASTERMIND\n\n"
+              "================================================\n"
+              "Test your code-cracking prowess with Mastermind,\n"
+              "the challenging game of logic and deduction.\n"
+              "================================================\n")
+
+    def display_home(self):
+        """
+        Displays home screen with options for playing the game or viewing
+        the leaderboard.
+        """
+        print("Welcome!\n"
+              "1. Play game\n"
+              "2. View leaderboard\n")
+        option = self.nv.get_home_screen(1, 2)
+        if option == 2:
+            Leaderboard.display_leaderboard()
+
+    def display_player_options(self, player: Player, developer_mode: bool) -> Player:
+        """
+        Displays player options and returns a new instance of a Player.
+        """
+        name = (self.sv.get_name() if not player
+                else player.name)
+        if name == "Tu" and not developer_mode:
+            developer_mode = self.av.get_credentials()
+        self.display_difficulty()
+        difficulty = self.nv.get_difficulty(1, 4)
+        return (Player(name, str(difficulty)), developer_mode)
+
+    def display_difficulty(self):
+        """
+        Displays the levels of difficulty.
+        """
+        print("\nDifficulty options:\n"
+              "1. Easy - 12 turns.\n"
+              "2. Normal - 10 turns.\n"
+              "3. Hard - 8 turns.\n"
+              "4. Dark Souls - 6 turns. 10 numbers to choose from.\n")
+
+    def display_instructions(self):
+        """
+        Displays game instructions.
+        """
+        print("\n================================================\n"
+              "Try to guess the correct number sequence by\n"
+              "entering four numbers.\n"
+              "================================================")
+
+    def display_endgame(self):
+        """
+        Displays end game options. Users can play again, view leaderboard
+        or quit.
+        """
+        while True:
+            print("\nWhat would you like to do next?\n"
+                  "1. Play again.\n"
+                  "2. View leaderboard.\n"
+                  "3. Quit.\n")
+            response = self.nv.get_endgame(1, 3)
+            if response == 1:
+                return True
+            elif response == 2:
+                Leaderboard.display_leaderboard()
+            else:
+                print("Goodbye!")
+                return False
